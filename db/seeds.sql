1d85d98c8cbb48ae3e96dc488f8680c5bdee6e4b
Joseph Cerankowski <joecerankowski@gmail.com>
2019-02-24 21:05:24 -0500

    set up structure of app

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+INSERT INTO burger (burger_name) VALUES ('Vegan');
+INSERT INTO burger (burger_name) VALUES ('Tofu');
+INSERT INTO burger (burger_name) VALUES ('Raw');
+
+INSERT INTO parties (party_name, party_type, party_cost, client_id) VALUES ('Everybody Loves Raymond', 'tv', 500, 1);
+INSERT INTO parties (party_name, party_type, party_cost, client_id) VALUES ('Big Bang Theory', 'tv', 900, 1);
+INSERT INTO parties (party_name, party_type, party_cost, client_id) VALUES ('Top Gun', 'movie', 200, 2);
+INSERT INTO parties (party_name, party_type, party_cost, client_id) VALUES ('Whiskey', 'grown-up', 300, 2);
+INSERT INTO parties (party_name, party_type, party_cost, client_id) VALUES ('Cigar', 'grown-up', 250, 3);

0439ba7e37ef2608d7ec9e869ddde657ec1d7f1d
Joseph Cerankowski <joecerankowski@gmail.com>
2019-03-01 20:53:43 -0500

    update

--- models/burgers.js
+++ models/burgers.js
@@ -7,11 +7,6 @@ var burgers = {
       cb(res);
     });
   },
-  insert: function(cols, vals, cb){
-      orm.insert("burgers", cols, vals, function(res){
-          cb(res)
-      })
-  },
   create: function(cols, vals, cb) {
     orm.create("burgers", cols, vals, function(res) {
       cb(res);

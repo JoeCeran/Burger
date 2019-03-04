b06f67465301ff1ce93a523a29f34c31962484b1
Joseph Cerankowski <joecerankowski@gmail.com>
2019-03-04 16:15:22 -0500

    updated image

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,10 @@
 var mysql = require("mysql");
 var connection;
 
-
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
   connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
@@ -10,6 +13,7 @@ var connection;
   password: "root",
   database: "burgers_db"
 });
+};
 
 // Make connection.
 connection.connect(function(err) {

bc57b628dd24b94ae2421610d48a05c6009b9029
Joseph Cerankowski <joecerankowski@gmail.com>
2019-03-04 15:59:18 -0500

    updated image

--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -16,23 +16,20 @@ $(function() {
     );
   });
 
-  $(".create-form").on("submit", function(event) {
-    // Make sure to preventDefault on a submit event.
+  $(".create-form").on("submit", function (event) {
+
     event.preventDefault();
 
     var newBurger = {
-      burger_name: $("#bu").val().trim()
+      burger_name: $("#bg").val().trim()
     };
-    console.log(newBurger);
 
     // Send the POST request.
     $.ajax("/api/burgers", {
       type: "POST",
       data: newBurger
     }).then(
-      function() {
-        console.log("created new burger");
-        // Reload the page to get the updated list
+      function () {
         location.reload();
       }
     );
